7d08abe15edc96fdd9a3f120290eebf033fcb1a0
Laslaa <mohamed.laslaa@gmail.com>
2020-01-29 09:30:47 +0100

    29012020 before modif constructor

--- classes/user.php
+++ classes/user.php
@@ -1,49 +1,75 @@
 <?php
 
-class Users {
+class Users
+{
 
     private $name;
     private $firstname;
     private $email;
     private $phone;
     private $password;
+    private $errors = [];
 
-    function __construct($name, $firstname, $email, $phone, $password)
+    function __construct($donnee)
     {
-        $this->name = htmlspecialchars($name);
-        $this->firstname = htmlspecialchars($firstname);
-        $this->email = htmlspecialchars($email);
-        $this->phone = htmlspecialchars($phone);
-        $this->password = htmlspecialchars($password);
-    }
-
-
-    function save(){
-        $y = json_decode(file_get_contents('user.json'), true);
-        $resId = array_column($y, 'id');
-        if(!empty($resId)) {
-            $Id = max($resId)+1;
-        }else{
-            $Id = 0;
+        if ($name !== "" && $firstname !== "") {
+            $this->name = htmlspecialchars($name);
+            $this->firstname = htmlspecialchars($firstname);
+            if (preg_match('/^\w(\d|\w|[.]){0,30}[@](\d|\w|[.]){0,10}[.](\d|\w){2,4}/', $email)) {
+                $this->email = htmlspecialchars($email);
+                if (preg_match('/^\d{10}/', $phone)) {
+                    $this->phone = htmlspecialchars($phone);
+                    if (strlen($password) >= 8) {
+                        $this->password = htmlspecialchars($password);
+                    } else {
+                        array_push($this->errors, 'Mot de passe incorrect');
+                    }
+                } else {
+                    array_push($this->errors, 'Numéro de téléphone invalide');
+                }
+            } else {
+                array_push($this->errors, 'Email Invalide');
+            }
+        } else {
+            array_push($this->errors, 'Tous les champs doivent être compléter');
         }
-        $x = [[
-            "id" => $Id,
-            "name" => $this->name,
-        "firstname" => $this->firstname,
-        "email" => $this->email,
-        "phone" => $this->phone,
-        "password" => $this->password]];
-        
-        $z = array_merge($y, $x);
-        file_put_contents('user.json', json_encode($z));
     }
 
-    public static function getAllUser(){
-        
+
+    function save()
+    {
+        if (!empty(array_filter($this->errors)) == 1) {
+            foreach ($this->errors as $value) {
+                echo "<p class='text-white bg-danger text-center mt-5 p-3'>" . $value .  "</p>";
+                $this->errors = [];
+            }
+        } else {
+            $y = json_decode(file_get_contents('user.json'), true);
+            $resId = array_column($y, 'id');
+            if (!empty($resId)) {
+                $Id = max($resId) + 1;
+            } else {
+                $Id = 0;
+            }
+            $x = [[
+                "id" => $Id,
+                "name" => $this->name,
+                "firstname" => $this->firstname,
+                "email" => $this->email,
+                "phone" => $this->phone,
+                "password" => $this->password
+            ]];
+            $z = array_merge($y, $x);
+            file_put_contents('user.json', json_encode($z));
+        }
     }
 
-    public static function getUserById(){
+    public static function getAllUser()
+    {
+    }
 
+    public static function getUserById()
+    {
     }
 
     public function getPhone()
@@ -82,7 +108,7 @@ class Users {
 
         return $this;
     }
- 
+
     public function getFirstname()
     {
         return $this->firstname;
@@ -105,5 +131,15 @@ class Users {
         $this->name = $name;
 
         return $this;
+    }
+
+    public function hydrate(array $donnees)
+    {
+        foreach ($donnees as $key => $value) {
+            $method = 'set' . ucfirst($key);
+            if (method_exists($this, $method)) {
+                $this->$method($value);
+            }
+        }
     }
 }
--- form.php
+++ form.php
@@ -20,23 +20,23 @@ require_once('classes/user.php')
         <form method="post">
             <div class="form-group">
                 <label for="name">Nom</label>
-                <input type="text" class="form-control" name="name" id="name" aria-describedby="emailHelp" placeholder="Nom" required>
+                <input type="text" class="form-control" name="name" id="name" aria-describedby="emailHelp" placeholder="Nom">
             </div>
             <div class="form-group">
                 <label for="firstname">Prénom</label>
-                <input type="text" class="form-control" name="firstname" id="firstname" aria-describedby="emailHelp" placeholder="Prénom" required>
+                <input type="text" class="form-control" name="firstname" id="firstname" aria-describedby="emailHelp" placeholder="Prénom">
             </div>
             <div class="form-group">
                 <label for="email">Email</label>
-                <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Enter email" required>
+                <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Enter email">
             </div>
             <div class="form-group">
                 <label for="phone">Téléphone</label>
-                <input type="number" class="form-control" name="phone" id="phone" aria-describedby="emailHelp" placeholder="Téléphone" required>
+                <input type="number" class="form-control" name="phone" id="phone" aria-describedby="emailHelp" placeholder="Téléphone">
             </div>
             <div class="form-group">
                 <label for="password">Mot de passe</label>
-                <input type="password" class="form-control" name="password" id="password" placeholder=">Mot de passe" required>
+                <input type="password" class="form-control" name="password" id="password" placeholder=">Mot de passe">
             </div>
             <input type="submit" class="btn btn-primary" name="submitted" value="Envoyer">
         </form>
@@ -44,10 +44,9 @@ require_once('classes/user.php')
         <?php
 
         if (isset($_POST['submitted'])) {
-            $x = new Users($_POST['name'], $_POST['firstname'], $_POST['email'], $_POST['phone'], $_POST['password']);
+            $x = new Users($_POST);
             $x->save();
         }
-
 
         ?>
 
